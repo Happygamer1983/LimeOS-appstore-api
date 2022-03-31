35e4467cfdbdc589a0559ebeb8ed7dd8b76eb541
XG213 <101302347+XG213@users.noreply.github.com>
2022-04-01 10:55:22 +1300

    app_name2

---
+++ app_name2.lua
@@ -0,0 +1,8 @@
+local app = createapp("code")
+local frame = new("Frame",app)
+frame.Size = UDim2.fromScale(1,1)
+local main_text = new("TextLabel",app)
+main_text.Size = UDim2.fromScale(1,0.8)
+main_text.Position = UDim2.fromScale(0,0)
+main_text.TextScaled = true
+main_text.Text = "code2"
